6e82e8584f186a42c12caa7914ad54e146b39d9f
Chris Stanton <chrisstanton@Chriss-MacBook-Pro.local>
2017-02-08 18:29:24 -0600

    updated js

--- client.js
+++ client.js
@@ -1,1 +1,18 @@
 
+
+
+function prime(a, b) {
+
+    if (a > b) {
+      return "The first number was bigger!";
+      //console.log("The first number was bigger!");
+  } else if (a == b) {
+      return "The numbers are the same!";
+      //console.log("The numbers are the same!");
+  } else (a < b)
+      return "The second number was bigger!";
+      //console.log("The second number was bigger!");
+}
+
+
+console.log(prime(2,1));
